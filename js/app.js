ef80a8a3c6666b735b4d60313865251ed68e24a4
bashar adel taamneh <bashartaamneh55@gmail.com>
2021-07-05 03:34:37 +0300

    edit coding

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ let midImg = document.getElementById('midil');
 let rigImg = document.getElementById('right');
 let resEl = document.getElementById('res');
 let purchas = [];
-let attempts = 1;
+let attempts = 0;
 let maxAttempts = 25;
 
 let purchasImg = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'water-can.jpg', 'wine-glass.jpg'];
@@ -16,8 +16,11 @@ let purchasImg = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfa
 function Survay(purchaseName) {
     this.purchaseName = purchaseName.split('.')[0];
     this.img = 'img/' + purchaseName;
+    this.views = 0;
     this.votes = 0;
+
     purchas.push(this);
+
 }
 
 for (let i = 0; i < purchasImg.length; i++) {
@@ -47,6 +50,10 @@ function renderRanImg() {
     lefImg.setAttribute('src', purchas[LImg].img);
     midImg.setAttribute('src', purchas[MImg].img);
     rigImg.setAttribute('src', purchas[RImg].img);
+    purchas[RImg].views++;
+    purchas[LImg].views++;
+    purchas[MImg].views++;
+
 }
 
 renderRanImg();
@@ -59,23 +66,41 @@ rigImg.addEventListener('click', clickshand);
 
 
 function clickshand(event) {
-    attempts++;
     if (attempts <= maxAttempts) {
-    let clicked = event.target.id;
-    console.log(clicked);
-    if (event.target.id === 'left') {
-        purchas[LImg].votes++;
-    }
+        let clicked = event.target.id;
+        console.log(clicked);
+        if (event.target.id === 'left') {
+            purchas[LImg].votes++;
+        }
 
-    else if (event.target.id === 'midil') {
-        purchas[LImg].votes++;
+        else if (event.target.id === 'midil') {
+            purchas[MImg].votes++;
+        }
+        else if (event.target.id === 'right') {
+            purchas[RImg].votes++;
+        }
+        renderRanImg();
     }
-    else if (event.target.id === 'right') {
-        purchas[LImg].votes++;
+    else {
+        let ulEl = document.getElementById('res');
+        for (let i = 0; i < purchas.length; i++) {
+            let liEl = document.createElement('li');
+            liEl.textContent = `${purchas[i].purchaseName} has ${purchas[i].votes} votes and ${purchas[i].views} views  .`
+            ulEl.appendChild(liEl);
+        }
+        alert("Thank you for your valuable time");
+        lefImg.removeEventListener('click', clickshand);
+        midImg.removeEventListener('click', clickshand);
+        rigImg.removeEventListener('click', clickshand);
+
     }
-    renderRanImg();
+    attempts++;
 }
-else{}
+
+function finish(){
+    window.location ='https://github.com/BasharTaamneh/BusMall/pull/1';
+
 }
- 
 console.log(purchas);
+
+
