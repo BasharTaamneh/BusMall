9e8da250c6af315dd628e0cfe35dc1d80c29551a
bashar adel taamneh <bashartaamneh55@gmail.com>
2021-07-07 18:33:36 +0300

    code inhanset

--- css/js/app.js
+++ css/js/app.js
@@ -163,7 +163,7 @@ function resrender() {
 }
 
 function finish() {
-    window.location = 'https://github.com/BasharTaamneh/BusMall/pull/1';
+    window.location = 'https://bashartaamneh.github.io/BusMall/#fin';
 }
 
 function savToLocStor() {
